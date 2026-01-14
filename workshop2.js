935887e05ad6efe0a906698b3d9968daebc3e209
Thanyathon86 <thanyathonwong19452@gmail.com>
2026-01-14 14:46:11 +0700

    worksop 2,3,4

---
+++ workshop2.js
@@ -0,0 +1,14 @@
+let mystring = "Hello, World!"; // ตั้งตัวเเปร string //
+let mynumber = 42; // ตั้งตัวเเปร number //
+let myboolean = true; // ตั้งตัวเเปร boolean //
+let mynull = null; // ตั้งตัวเเปร null //
+let myunderfined = undefined; // ตั้งตัวเเปร undefined //
+
+console.log("type of hello:",typeof mystring); // ทำการปริ้นขตัวเเปรเเละเช็คประภาท // 
+console.log("type of 42:", typeof mynumber);
+console.log("type of true:",typeof myboolean);
+console.log("type of null:", typeof mynull);
+console.log("type of undefined:", typeof myunderfined);
+
+
+
